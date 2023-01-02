8073739dd48cf52e4bbedecd9e8c3f7292d9cabd
Vitor Paulo <vitorblog12@gmail.com>
2023-01-02 18:38:38 -0300

    Adding log time request

--- src/app/service/kanbanize.service.ts
+++ src/app/service/kanbanize.service.ts
@@ -16,6 +16,11 @@ export class KanbanizeService {
     return this.http.post(this.mountUrl(url, 'login'), {}, this.generateOptions(''));
   }
 
+  getLogTime(request: GetLogTimeRequest): Observable<GetLogTimeResponse[]> {
+    return this.http.post<GetLogTimeResponse[]>(
+      this.mountUrl('', 'get_log_time_activities'), request, this.generateOptions(''));
+  }
+
   mountUrl(url: string, functionName: string): string {
     return 'https://' + url + this.basePath + functionName;
   }
@@ -27,5 +32,48 @@ export class KanbanizeService {
         'apikey': key
       }
     };
+  }
+}
+
+export class GetLogTimeRequest {
+  fromdate: Date;
+  todate: Date;
+  author: string;
+
+  constructor(fromdate: Date, todate: Date, author: string) {
+    this.fromdate = fromdate;
+    this.todate = todate;
+    this.author = author;
+  }
+}
+export class GetLogTimeResponse {
+  id: string;
+  date: Date;
+  author: string;
+  cardid: string;
+  boardid: string;
+  cardtype: string;
+  cardtitle: string;
+  loggedtime: number;
+  columnname: string;
+  description: string;
+  loggedtimeid: number;
+  loggedinsubtask: boolean;
+  loggedinsubtaskid: string | null;
+
+  constructor(id: string, date: Date, author: string, cardid: string, boardid: string, cardtype: string, cardtitle: string, loggedtime: number, columnname: string, description: string, loggedtimeid: number, loggedinsubtask: boolean, loggedinsubtaskid: string | null) {
+    this.id = id;
+    this.date = date;
+    this.author = author;
+    this.cardid = cardid;
+    this.boardid = boardid;
+    this.cardtype = cardtype;
+    this.cardtitle = cardtitle;
+    this.loggedtime = loggedtime;
+    this.columnname = columnname;
+    this.description = description;
+    this.loggedtimeid = loggedtimeid;
+    this.loggedinsubtask = loggedinsubtask;
+    this.loggedinsubtaskid = loggedinsubtaskid;
   }
 }
