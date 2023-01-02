1a2669b7dd1037d375a33073964c2502b21be737
Vitor Paulo <vitorblog12@gmail.com>
2023-01-02 18:18:21 -0300

    Creating button basic logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {faInfoCircle} from "@fortawesome/free-solid-svg-icons";
+import * as bulmaToast from 'bulma-toast'
+import {toast} from "bulma-toast";
 
 @Component({
   selector: 'app-login',
@@ -19,6 +21,36 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this.loading = this.validate();
+  }
+
+  validate(): boolean {
+    if (this.formData.key.length <= 4) {
+      toast(
+        {
+          message: 'Your api key ins\'t valid.',
+          type: 'is-danger'
+        }
+      );
+
+      return false;
+    }
+
+    if (this.formData.board.length <= 4 || !this.formData.board.includes('kanbanize.com')) {
+      toast(
+        {
+          message: 'Your board url is invalid.',
+          type: 'is-danger'
+        }
+      );
+
+      return false;
+    }
+
+    return true;
   }
 
 }
