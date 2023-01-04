e78564b14c67ec931af420639362e412fad02c75
Vitor Paulo <vitorblog12@gmail.com>
2023-01-04 10:51:15 -0300

    Adding auth page select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./service/auth.service";
+import {KanbanizeService} from "./service/kanbanize.service";
+import {toast} from "bulma-toast";
+import {UserDataModel} from "./model/userData.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  isLogged = false;
+  isLogged = this.authService.getUserData() != undefined;
+  userData: UserDataModel | undefined;
+
+  constructor(private authService: AuthService, private kanbanizeService: KanbanizeService) {
+  }
+
+  ngOnInit() {
+    const userData = this.authService.getUserData();
+    if (!userData) {
+      this.isLogged = false;
+    } else {
+      this.kanbanizeService.getUser(userData.board, userData.key).subscribe(
+        (response) => {
+          this.authService.saveUserData(userData.key, userData.board, response.data);
+          this.userData = this.authService.getUserData();
+        },
+        () => {
+          toast(
+            {
+              message: 'An error occurred.',
+              type: 'is-danger'
+            }
+          );
+          this.authService.deleteUserData();
+          this.isLogged = false;
+        }
+      )
+    }
+  }
 }
