7f9da567020be3e11b424548e30e88acb66aaa3c
Vitor Paulo <vitorblog12@gmail.com>
2023-01-31 12:06:19 -0300

    Moving DTOs to proper folder and files

--- src/app/service/kanbanize.service.ts
+++ src/app/service/kanbanize.service.ts
@@ -3,6 +3,9 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {UserModel} from "../model/user.model";
 import {UserDataModel} from "../model/userData.model";
+import {UserResponse} from "../model/userResponse.model";
+import {LoggedTimeResponse} from "../model/loggedTimeResponse.model";
+import {GetLoggedTimeRequest} from "../model/getLoggedTimeRequest.model";
 
 @Injectable({
   providedIn: 'root',
@@ -12,8 +15,7 @@ export class KanbanizeService {
   private BASE_PATH = '/api/v2/';
   private DEPRECATED_BASE_PATH = '/index.php/api/kanbanize/';
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   getUser(url: string, key: string): Observable<UserResponse> {
     return this.http.get<UserResponse>(
@@ -36,56 +38,5 @@ export class KanbanizeService {
         'accept': 'application/json'
       }
     };
-  }
-}
-export class UserResponse {
-  data: UserModel;
-
-  constructor(data: UserModel) {
-    this.data = data;
-  }
-}
-
-export class LoggedTimeResponse {
-  id: string;
-  date: string;
-  author: string;
-  cardid: string;
-  boardid: string;
-  cardtype: string;
-  cardtitle: string;
-  columnname: string;
-  loggedtime: number;
-  description: string;
-  loggedtimeid: number;
-  loggedinsubtaskid: any;
-  loggedinsubtask: boolean;
-
-  constructor(id: string, date: string, author: string, cardid: string, boardid: string, cardtype: string, cardtitle: string, columnname: string, loggedtime: number, description: string, loggedtimeid: number, loggedinsubtaskid: any, loggedinsubtask: boolean) {
-    this.id = id;
-    this.date = date;
-    this.author = author;
-    this.cardid = cardid;
-    this.boardid = boardid;
-    this.cardtype = cardtype;
-    this.cardtitle = cardtitle;
-    this.columnname = columnname;
-    this.loggedtime = loggedtime;
-    this.description = description;
-    this.loggedtimeid = loggedtimeid;
-    this.loggedinsubtaskid = loggedinsubtaskid;
-    this.loggedinsubtask = loggedinsubtask;
-  }
-}
-
-export class GetLoggedTimeRequest {
-  fromdate: string;
-  todate: string;
-  author: string;
-
-  constructor(fromdate: Date, todate: Date, author: string) {
-    this.fromdate = fromdate.toISOString().split('T')[0];
-    this.todate = todate.toISOString().split('T')[0];
-    this.author = author;
   }
 }
